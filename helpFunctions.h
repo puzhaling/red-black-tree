c37b3b2b6d594274963a481b8489ca4e1f6052b9
Hermann Puzhalin <puzhaling@gmail.com>
2023-11-02 21:17:41 +1000

    feat: change function signature
    - for better readability

--- helpFunctions.h
+++ helpFunctions.h
@@ -4,37 +4,18 @@
 #include "libraries.h"
 #include "RBTreeClass.h"
 
-Carplate*
-getCarplate(const std::string& text);
+Carplate* getCarplate(const std::string& text);
 
-void
-printList(ListNode* head);
+void printList(ListNode* head);
+void inorderHelper(Node* root);
+void inorder(RBTree* tree);
+void narrowList(ListNode* head);
+void deleteList(ListNode* &head);
+void deleteTreeHelper(Node* &root);
+void deleteTree(RBTree* &tree);
 
-void
-inorderHelper(Node* root);
-
-void
-inorder(RBTree* tree);
-
-bool
-isLeaf(Node* root);
-
-bool
-isLastInList(ListNode* head);
-
-void
-narrowList(ListNode* head);
-
-void
-deleteList(ListNode* &head);
-
-void
-deleteTreeHelper(Node* &root);
-
-void
-deleteTree(RBTree* &tree);
-
-bool
-isvalid(const std::string& key);
+bool isLeaf(Node* root);
+bool isLastInList(ListNode* head);
+bool isvalid(const std::string& key);
 
 #endif
